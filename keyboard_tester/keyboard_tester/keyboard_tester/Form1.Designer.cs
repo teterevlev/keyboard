fb7ac8cd5bd85b0a9a10a64e742191a8ed4d43e8
teterevlev <o.damage@gmail.com>
2017-02-17 20:28:36 +0300

    Can open, close and log serial port

---
+++ keyboard_tester/keyboard_tester/keyboard_tester/Form1.Designer.cs
@@ -0,0 +1,203 @@
+﻿namespace keyboard_tester
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.serialPort1 = new System.IO.Ports.SerialPort(this.components);
+            this.timer2sec = new System.Windows.Forms.Timer(this.components);
+            this.portList = new System.Windows.Forms.ListBox();
+            this.openPortCheckbox = new System.Windows.Forms.CheckBox();
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.log = new System.Windows.Forms.RichTextBox();
+            this.Online = new System.Windows.Forms.ProgressBar();
+            this.label1 = new System.Windows.Forms.Label();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.checkBox2 = new System.Windows.Forms.CheckBox();
+            this.ProgramButton = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.tableLayoutPanel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // serialPort1
+            // 
+            this.serialPort1.BaudRate = 115200;
+            this.serialPort1.DataReceived += new System.IO.Ports.SerialDataReceivedEventHandler(this.serialPort1_DataReceived);
+            // 
+            // timer2sec
+            // 
+            this.timer2sec.Enabled = true;
+            this.timer2sec.Interval = 2000;
+            this.timer2sec.Tick += new System.EventHandler(this.timer2sec_Tick);
+            // 
+            // portList
+            // 
+            this.portList.FormattingEnabled = true;
+            this.portList.Location = new System.Drawing.Point(12, 23);
+            this.portList.Name = "portList";
+            this.portList.Size = new System.Drawing.Size(75, 43);
+            this.portList.TabIndex = 15;
+            this.portList.SelectedValueChanged += new System.EventHandler(this.portList_SelectedValueChanged);
+            // 
+            // openPortCheckbox
+            // 
+            this.openPortCheckbox.AutoSize = true;
+            this.openPortCheckbox.Location = new System.Drawing.Point(12, 72);
+            this.openPortCheckbox.Name = "openPortCheckbox";
+            this.openPortCheckbox.Size = new System.Drawing.Size(75, 17);
+            this.openPortCheckbox.TabIndex = 16;
+            this.openPortCheckbox.Text = "Auto open";
+            this.openPortCheckbox.UseVisualStyleBackColor = true;
+            this.openPortCheckbox.CheckedChanged += new System.EventHandler(this.openPortCheckbox_CheckedChanged);
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.AutoSize = true;
+            this.tableLayoutPanel1.ColumnCount = 1;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.Controls.Add(this.Online, 0, 0);
+            this.tableLayoutPanel1.Controls.Add(this.log, 0, 0);
+            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Right;
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(220, 0);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 1;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 97.02602F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(335, 482);
+            this.tableLayoutPanel1.TabIndex = 22;
+            // 
+            // log
+            // 
+            this.log.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.log.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.log.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.log.ForeColor = System.Drawing.Color.Silver;
+            this.log.Location = new System.Drawing.Point(3, 3);
+            this.log.Name = "log";
+            this.log.ReadOnly = true;
+            this.log.Size = new System.Drawing.Size(329, 456);
+            this.log.TabIndex = 14;
+            this.log.Text = "";
+            // 
+            // Online
+            // 
+            this.Online.Location = new System.Drawing.Point(3, 465);
+            this.Online.Name = "Online";
+            this.Online.Size = new System.Drawing.Size(329, 10);
+            this.Online.Step = 100;
+            this.Online.TabIndex = 23;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(9, 7);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(54, 13);
+            this.label1.TabIndex = 24;
+            this.label1.Text = "Serial port";
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Checked = true;
+            this.checkBox1.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.checkBox1.Location = new System.Drawing.Point(127, 72);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(54, 17);
+            this.checkBox1.TabIndex = 25;
+            this.checkBox1.Text = "Fuses";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // checkBox2
+            // 
+            this.checkBox2.AutoSize = true;
+            this.checkBox2.Checked = true;
+            this.checkBox2.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.checkBox2.Location = new System.Drawing.Point(127, 52);
+            this.checkBox2.Name = "checkBox2";
+            this.checkBox2.Size = new System.Drawing.Size(68, 17);
+            this.checkBox2.TabIndex = 26;
+            this.checkBox2.Text = "Firmware";
+            this.checkBox2.UseVisualStyleBackColor = true;
+            // 
+            // ProgramButton
+            // 
+            this.ProgramButton.Location = new System.Drawing.Point(127, 23);
+            this.ProgramButton.Name = "ProgramButton";
+            this.ProgramButton.Size = new System.Drawing.Size(75, 23);
+            this.ProgramButton.TabIndex = 27;
+            this.ProgramButton.Text = "Program";
+            this.ProgramButton.UseVisualStyleBackColor = true;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(124, 7);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(63, 13);
+            this.label2.TabIndex = 28;
+            this.label2.Text = "Programmer";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSize = true;
+            this.ClientSize = new System.Drawing.Size(555, 482);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.ProgramButton);
+            this.Controls.Add(this.checkBox2);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.Controls.Add(this.openPortCheckbox);
+            this.Controls.Add(this.portList);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.IO.Ports.SerialPort serialPort1;
+        private System.Windows.Forms.Timer timer2sec;
+        private System.Windows.Forms.ListBox portList;
+        private System.Windows.Forms.CheckBox openPortCheckbox;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.ProgressBar Online;
+        private System.Windows.Forms.RichTextBox log;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.CheckBox checkBox2;
+        private System.Windows.Forms.Button ProgramButton;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ keyboard_tester/keyboard_tester/keyboard_tester/Form1.cs
@@ -0,0 +1,167 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO.Ports;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace keyboard_tester
+{
+    public partial class Form1 : Form
+    {
+        public delegate void AddDataDelegate(String myString);
+        public AddDataDelegate myDelegate;
+        string oldPortName = "";
+        string portName="";
+        bool openPortText = true;
+        public Form1()
+        {
+            InitializeComponent();
+            this.myDelegate = new AddDataDelegate(AddDataMethod);
+            updatePortList();
+        }
+
+        private void updatePortName(){
+            if (portList.Items.Contains(portName))
+            {
+                portList.SelectedItem = portName;
+            }
+            else
+            {
+                portName = "";
+            }
+            
+        }
+        private void updatePortList()
+        {
+            //toLog("updatePortList\n");
+            bool once = true;
+            string[] ports = SerialPort.GetPortNames();
+            portList.Items.Clear();
+            once = true;
+            foreach (string port in ports)
+            {
+                if (once || port != portName)
+                {
+                    portList.Items.Add(port);
+                }
+                if(port == portName)
+                {
+                    once = false;
+                }
+            }
+            
+        }
+        public bool portNameChanged()
+        {
+            int index = portList.SelectedIndex;
+            if (index >= 0)
+            {
+                portName = portList.Items[index].ToString();
+            }
+            if (portName != oldPortName)
+            {
+                oldPortName = portName;
+                return true;
+            }
+            return false;
+        }
+        private void reopenPort()
+        {
+            closePort();
+            openPort();
+        }
+        private void checkPortState()
+        {
+
+            Online.Value = serialPort1.IsOpen ? 100 : 0;
+
+        }
+        private void timer2sec_Tick(object sender, EventArgs e)
+        {
+            updatePortList();
+            updatePortName();
+            if (portNameChanged()){
+                reopenPort();
+            }
+            if (!serialPort1.IsOpen && openPortCheckbox.Checked)
+            {
+                //toLog(portName+"has been disconnected!!!\n");
+                closePort();
+                openPort();
+            }
+            checkPortState();
+        }
+        private void portList_SelectedValueChanged(object sender, EventArgs e)
+        {
+            // обновить название порта
+            // закрыть старый и открыть новый порт
+            if (portNameChanged())
+            {
+                reopenPort();
+            }
+        }
+        private void openPortCheckbox_CheckedChanged(object sender, EventArgs e)
+        {
+            if (openPortCheckbox.Checked)
+            {
+                openPort();
+            }
+            else
+            {
+                closePort();
+            }
+        }
+        private void serialPort1_DataReceived(object sender, SerialDataReceivedEventArgs e)
+        {
+            SerialPort sp = (SerialPort)sender;
+            string s = sp.ReadExisting();
+            log.Invoke(this.myDelegate, new Object[] { s });
+        }
+        public void AddDataMethod(String myString)
+        {
+
+            toLog(myString);
+        }
+        public void closePort()
+        {
+            serialPort1.Close();
+            if (!openPortText)
+            {
+                toLog("Port closed\n", Color.Tomato);
+                openPortText = true;
+            }
+        }
+        public void openPort()
+        {
+            if (portName != "" && openPortCheckbox.Checked)
+            {
+                serialPort1.PortName = portName;
+                try
+                {
+                    serialPort1.Open();
+                    openPortText = false;
+                    toLog("Port " + portName + " open\n", Color.Tomato);
+                }
+                catch (Exception exc)
+                {
+                    //Console.WriteLine("Error opening port: {0}", ex.Message);
+                }
+                
+            }
+        }
+
+
+        private void toLog(string s, Color ? color = null)
+        {
+
+            log.SelectionColor = color ?? Color.Silver;
+            log.AppendText(s);
+            log.ScrollToCaret();
+        }
+    }
+}
